2062ed4e074034e56c8df64229c41ba2d0d776d3
Allan Brockenbrough <brockenbrough@gmail.com>
2023-01-05 10:02:38 -0500

    Added call to MBTA Alerts.  Created page.

---
+++ frontend/src/components/pages/mbtaAlerts.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function Alerts() {
+  const [alerts, setAlerts] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const result = await axios(
+        'https://api-v3.mbta.com/alerts?sort=banner&filter%5Bactivity%5D=BOARD%2CEXIT%2CRIDE',
+      );
+      setAlerts(result.data.data);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+        <h1>Alerts!</h1>
+      {alerts.map(alert => (
+        <div key={alert.id}>
+          <h3>{alert.attributes.header}</h3>
+          <p>{alert.attributes.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Alerts;
